efc877bc02c8843d6ae2f68653290a31723185fa
Vishnu Vardhan Reddy <91204654+9493vishnu@users.noreply.github.com>
2023-11-27 20:26:55 +0530

    Add files via upload

---
+++ Packages/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv"
+import mongoose from 'mongoose';
+import userRoute from './routes/users.js'
+import roomsRoute from './routes/rooms.js'
+import cors from "cors"
+const app=express()
+dotenv.config()
+
+
+const connect=async ()=>{
+try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to Mongo")
+  }
+   catch (error) {
+   throw error;
+  }
+};
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/user",userRoute)
+app.use("/api/room",roomsRoute)
+
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to my Hotel Api")
+})
+
+
+
+connect().then(()=>{
+app.listen(8800,()=>{
+    console.log("Connected to Backend");
+})})
+.catch((err)=>{
+    console.log(err);
+})
